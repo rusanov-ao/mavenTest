ef73b3b3da658db12b130d7600f8e1e84171cb19
rusanov-ao <rusanov-ao@inbox.ru>
2022-05-03 20:58:59 +0300

    one commit

---
+++ src/main/java/JsonStudy/Cat.java
@@ -0,0 +1,7 @@
+package JsonStudy;
+
+public class Cat {
+    public String name;
+    public int age;
+    public int color;
+}
--- src/main/java/JsonStudy/Main.java
+++ src/main/java/JsonStudy/Main.java
@@ -2,13 +2,18 @@ package JsonStudy;
 
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import javax.management.OperationsException;
 import javax.xml.transform.stream.StreamResult;
+import java.awt.*;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
@@ -24,24 +29,64 @@ public class Main {
 //            ex.printStackTrace();
 //        }
 
-        JSONParser parser = new JSONParser();
-        try {
-            Object obj = parser.parse(new FileReader("data.json"));
-            JSONObject jsonObject = (JSONObject) obj;
 
-            String lastName = (String) jsonObject.get("lastName");
-            System.out.println(lastName);
+        // чтение отдельных элементов
+//        JSONParser parser = new JSONParser();
+//        try {
+//            Object obj = parser.parse(new FileReader("data.json"));
+//            JSONObject jsonObject = (JSONObject) obj;
+//
+//            String lastName = (String) jsonObject.get("lastName");
+//            System.out.println(lastName);
+//
+//            JSONObject address = (JSONObject) jsonObject.get("address");
+//            String streetAddress = (String) address.get("streetAddress");
+//            System.out.println(streetAddress);
+//
+//            JSONArray phoneNumbers = (JSONArray) jsonObject.get("phoneNumbers");
+//            for (Object number : phoneNumbers) {
+//                System.out.println(number);
+//            }
+//        } catch (IOException | ParseException e) {
+//            e.printStackTrace();
+//        }
 
-            JSONObject address = (JSONObject) jsonObject.get("address");
-            String streetAddress = (String) address.get("streetAddress");
-            System.out.println(streetAddress);
 
-            JSONArray phoneNumbers = (JSONArray) jsonObject.get("phoneNumbers");
-            for (Object number : phoneNumbers) {
-                System.out.println(number);
-            }
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
+        // создать json и записать в него
+//        JSONObject obj = new JSONObject();
+//        obj.put("name", "mkyong.com");
+//        obj.put("age", 100);
+//        obj.put("boolean", true);
+//        obj.put("booleanString", "true");
+//        obj.put("null", null);
+//        obj.put("nullString", "null");
+//
+//        JSONArray list = new JSONArray();
+//        list.add("msg 1");
+//        list.add("msg 2");
+//        list.add("msg 3");
+//
+//        obj.put("message", list);
+//
+//        try (FileWriter file = new FileWriter("new_data.json")) {
+//            file.write(obj.toJSONString());
+//            file.flush(); // гарантия что буфер будет записан
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        // сериализация
+//        Cat cat = new Cat();
+//        cat.name = "Матроскин";
+//        cat.age = 5;
+//        cat.color = Color.blue.getRGB();
+//
+//        GsonBuilder builder = new GsonBuilder();
+//        Gson gson = builder.create();
+//        System.out.println(gson.toJson(cat));
+
+        String jsonText = "{\"name\":\"Мурзик\",\"color\":-16777216,\"age\":9}";
+
+        GsonBuilder builder = new GsonBuilder();
     }
 }
